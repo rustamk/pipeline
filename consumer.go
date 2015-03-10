f6bfcf3c17502ba048ee2bbac4c6e504e36ca5b9
David Arbuckle <david_arbuckle@cable.comcast.com>
2015-03-10 09:29:51 -0600

    Implemented Kafka consumer w/ channel to Decorator.
    All communication within this app is mediated through two channels.
    
    raw, and decorated.
    
    The Kafka consumers retrieve messages from Kafka and drop them into the
    raw channel.  A decorator goroutine reads from the raw channel and
    performs a non-blocking retrieval of metric metadata from the decorator
    service / local cache.  The decorator builds a key:value JSON structure
    and drops it into the decorated channel.  Finally, a publisher goroutine
    reads from the decorated channel and writes out to Kafka.
    
    Some caveats:
    -  Kafka consumers have two modes of operation - group mode and offset
    mode.  The consumer as-is is using the offset mode, which means that
    multiple instances of this program would duplicate effort in the same
    Kafka topic.
    - The channels in use are unbuffered, meaning that if the decorator or
    publisher goroutines crash, the entire program will deadlock.
    - Testing this is a PITA, because the ingress into the program is via
    Kafka, meaning there's an integration dependency on another service.

---
+++ consumer.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.comcast.com/viper-sde/sarama"
+)
+
+// stub wrapper around sarama.Client
+type Client interface {
+	Partitions()
+	ConsumePartition()
+	GetOffset()
+}
+
+// Wrapper around sarama.PartitionConsumer
+type PartitionConsumer interface {
+	Messages() <-chan *sarama.ConsumerMessage
+	Errors() <-chan *sarama.ConsumerError
+	Close() error
+	AsyncClose()
+}
+
+// Creates a new Consumer with the given topic and client.
+func NewConsumer(topic string, outboundChan chan []byte, client *sarama.Client) (*Consumer, error) {
+	errors := make(chan error)
+	var c *Consumer = &Consumer{
+		errors:   errors,
+		outbound: outboundChan,
+		client:   client,
+		topic:    topic,
+	}
+
+	var err error
+	c.consumer, err = sarama.NewConsumerFromClient(client)
+	return c, err
+}
+
+// Consumer establishes a connection to Kafka and reads messages off of the queue, pushing them into Messages
+type Consumer struct {
+	errors   chan error
+	outbound chan []byte
+
+	err    error
+	topic  string
+	client *sarama.Client
+
+	consumer          *sarama.Consumer
+	partitionConsumer *sarama.PartitionConsumer
+}
+
+// Returns an unbuffered chan of errors through which errors can propagate.
+func (c *Consumer) Errors() chan error {
+	return c.errors
+}
+
+// Returns an unbuffered chan of raw message bytes.
+func (c *Consumer) Messages() chan []byte {
+	return c.outbound
+}
+
+// Kicks off a process to read data from kafka and write messages to an outbound queue.
+// This piece needs to kick off a partitionConsumer for each partition on the client,
+// and keep a record of all of the partitionConsumers that are running at any given time,
+// so that dead consumers can be restarted, and so that new partitions can be added.
+func (c *Consumer) Start() {
+
+	// Determine the number of Kafka partitions we need to read from.
+	partitions, err := c.client.Partitions(c.topic)
+	if err != nil {
+		c.err = err
+		return
+	}
+
+	// Initializing a partition consumer for each partition.  Kafka only supports adding partitions at runtime.
+	// If the number of Kafka partitions is changed during runtime, this will chug along unaffected, but
+	// data pushed into the new partitions will not be ingested.  The process will need to be restarted.
+	for partition := range partitions {
+		p := int32(partition)
+		offset, _ := c.client.GetOffset(c.topic, p, sarama.LatestOffsets)
+		partitionConsumer, _ := c.consumer.ConsumePartition(c.topic, p, offset)
+		fmt.Println("Creating Partition", partition)
+		go partitionListener(partitionConsumer, c.errors, c.outbound)
+		fmt.Println("Created Partition", partition)
+	}
+}
+
+// Retrieves messages from the partition consumer's messages chan and publishes bytes through rawCollectd.
+// On error, propagates the error to the creator via the errors channel.
+func partitionListener(pc PartitionConsumer, errChan chan<- error, rawCollectd chan<- []byte) {
+	for {
+		select {
+		case msg := <-pc.Messages():
+			fmt.Println(1)
+			rawCollectd <- msg.Value
+		case msg := <-pc.Errors():
+			errChan <- errors.New(msg.Error())
+			fmt.Println("dead")
+			return
+		default:
+			fmt.Println("aaa")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+	fmt.Println("dead")
+}
---
+++ pipeline.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.comcast.com/viper-sde/gocollectd"
+	"github.comcast.com/viper-sde/sarama"
+)
+
+const (
+	clientId string = "collectd-decorator"
+	//groupId        string = "collectd-decorator"
+	rawTopic       string = "test"
+	decoratedTopic string = "test-decorated"
+)
+
+var kafkaHost []string = []string{"192.168.59.103:9092"}
+
+// Creates a new decorator
+func NewDecorator(inbound chan []byte, outbound chan []byte) (*Decorator, error) {
+	cache := make(map[string]map[string]string)
+	cache["f1710c7cbd79"] = map[string]string{
+		"cluster":   "a",
+		"esxi_host": "10.22.222.2",
+		"docker":    "1",
+		"virtual":   "1",
+		"arbuckle":  "david",
+	}
+	return &Decorator{
+		inbound:  inbound,
+		outbound: outbound,
+		cache:    cache,
+	}, nil
+}
+
+type Decorator struct {
+	inbound  chan []byte
+	outbound chan []byte
+
+	cache map[string]map[string]string
+}
+
+// Returns a channel of decorated messages.
+func (d *Decorator) Messages() chan []byte {
+	return d.outbound
+}
+
+// Kicks off the decorator process.  Retrieves messages from the inbound queue, parses them,
+// decorates them, and pushes them into the outbound queue.
+func (d *Decorator) Start() {
+	for {
+		select {
+		case msg := <-d.inbound:
+			err := d.parseCollectdPacket(msg)
+			fmt.Println(err)
+		default:
+			fmt.Println("bbb")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+// Splits a collectd packet out into its constitutent messages.
+func (d *Decorator) parseCollectdPacket(b []byte) error {
+	var packets *[]gocollectd.Packet
+	var err error
+	if packets, err = gocollectd.Parse(b); err != nil {
+		return err
+	}
+
+	meta := map[string]string{}
+	for i, packet := range *packets {
+		// searching cache for host match.
+		// If not found, searching remote API.
+		if i == 0 {
+			if meta, err = d.GetHostData(packet.Hostname); err != nil {
+				if meta, err = d.GetRemoteHostData(packet.Hostname); err != nil {
+					meta["error"] = "error retrieving data"
+				}
+			}
+		}
+		fmt.Println(meta)
+		val, _ := packet.Values()[0].Number()
+		fmt.Println(i, packet.Hostname, packet.Name(), packet.TimeUnix(), packet.Plugin, packet.PluginInstance,
+			packet.Type, packet.TypeInstance, packet.ValueNames(), val)
+	}
+	return nil
+}
+
+// retrieves decoration string from the decorator's local cache.
+// Entries in the cache have a TTL of 5 minutes +- 150 seconds, after which the record
+// for the hostname will expire and the query will return an error.
+func (d *Decorator) GetHostData(hostname string) (map[string]string, error) {
+	if match, ok := d.cache[hostname]; ok == false {
+		return map[string]string{}, errors.New("cache miss")
+	} else {
+		return match, nil
+	}
+}
+
+// Retrieves decoration string from a remote API.
+func (d *Decorator) GetRemoteHostData(hostname string) (map[string]string, error) {
+	return make(map[string]string), nil
+}
+
+func main() {
+	config := sarama.NewConfig()
+	config.ClientID = clientId
+
+	client, err := sarama.NewClient(kafkaHost, config)
+	if err != nil {
+		panic(err)
+	}
+
+	cRaw := make(chan []byte)
+	cDecorated := make(chan []byte)
+
+	consumer, err := NewConsumer(rawTopic, cRaw, client)
+	if err != nil {
+		panic(err)
+	}
+
+	decorator, err := NewDecorator(consumer.Messages(), cDecorated)
+	if err != nil {
+		panic(err)
+	}
+
+	consumer.Start()
+	decorator.Start()
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+}
