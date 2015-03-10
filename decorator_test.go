b0734bac0df413ab0a8eb8b4159a5394fcff906d
David Arbuckle <david_arbuckle@cable.comcast.com>
2015-03-10 16:48:46 -0600

    Unit test(s) for decorator.

--- decorator_test.go
+++ decorator_test.go
@@ -4,17 +4,58 @@ import (
 	"testing"
 )
 
-func TestProducerMessage(t *testing.T) {
+func _decoratorMaker() (*Decorator, error, chan []byte, chan []byte) {
+	a := make(chan []byte)
+	b := make(chan []byte)
 
-	i := 1
-	result := getProducerMessage(i)
-	if result != "world hello 1" {
-		t.Error("Expected 'hello world 1'. Saw ", result)
+	d, err := NewDecorator(a, b)
+	return d, err, a, b
+}
+
+func TestNewDecoratorPopulatesCache(t *testing.T) {
+	d, err, _, _ := _decoratorMaker()
+	if err != nil {
+		t.Error("Unexpected error", err)
 	}
-
-	i = 2
-	result = getProducerMessage(i)
-	if result != "hello world 2" {
-		t.Error("Expected 'world hello 1'. Saw ", result)
+	if d.cache == nil {
+		t.Error("Expected decorator cache to be initialized")
 	}
 }
+
+func TestDecoratorGetsHostDataFromCache(t *testing.T) {
+	var d *Decorator
+	var err error
+	if d, err, _, _ = _decoratorMaker(); err != nil {
+		t.Fatal("unexpected error retrieving cached data", err)
+	}
+	d.cache["arbuckle"] = map[string]interface{}{
+		"testing_this": true,
+	}
+	if _, err := d.GetHostData("arbuckle"); err != nil {
+		t.Fatal("unexpected error retrieving cached data", err)
+	}
+}
+
+func TestDecoratorChokesOnBadHostname(t *testing.T) {
+	var d *Decorator
+	var err error
+	if d, err, _, _ = _decoratorMaker(); err != nil {
+		t.Fatal("unexpected error retrieving cached data", err)
+	}
+	if _, err := d.GetHostData(""); err == nil {
+		t.Error("Uncaught error when invalid hostname provided")
+	}
+}
+
+// TODO:  some more tests:
+// Decorator takes valid payload through inbound and to outbound channels
+// Decorator appends internal errors to a private field when decoration fails
+// Decorator accepts configuration object / API host as a dependency on creation
+// Decorator doesn't block when outbound queue isn't read
+
+// Refactoring / remaining tasks
+// Clean up parseCollectdPacket.  Return all packets in an array.
+// Test harness server for decorator HTTP requests
+// Unit tests for inbound data.
+// File-based capture of collectd streaming data for testing purposes
+// Producer component.
